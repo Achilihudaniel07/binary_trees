e77bef75b74996cbdef0400269785bbc734b4d56
Achilihu Daniel Uchenna <achilihudaniel53@gmail.com>
2023-07-20 20:39:15 +0100

    task

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,1 +1,15 @@
-binary trees
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - Deletes a binary tree.
+ * @tree: A pointer to the root node of the tree to delete.
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree != NULL)
+	{
+		binary_tree_delete(tree->left);
+		binary_tree_delete(tree->right);
+		free(tree);
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-0x1D. C - Binary trees
+# binary_trees
